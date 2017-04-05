0219b1a57fe2832e0898a33b71939dfbc7a8d49c
Parnswir <maximiliangrundke@googlemail.com>
2017-04-05 16:32:22 +0200

    Move map

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require([
     '../../map'
   ],
   (TileEngine, MapLoader) => {
-    (new MapLoader).load('map.json').then((map) => {
+    (new MapLoader).load('assets/maps/house.json').then((map) => {
         let tileEngine = new TileEngine(0, 0, map.height, map.width);
         tileEngine.init(map.layers);
     });

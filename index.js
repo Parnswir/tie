ca338e5d014047b73d2b1ceb91f2fde15b274c6b
Parnswir <maximiliangrundke@googlemail.com>
2017-04-05 11:42:55 +0200

    Remove useless scoping

--- index.js
+++ index.js
@@ -8,38 +8,33 @@ require([
     '../../engine'
   ],
   function(imgLoader, jsonLoader, TileEngine) {
+    jsonLoader(['map.json']).then(function(maps) {
+      let map = maps[0];
 
-    function launch() {
-      jsonLoader(['map.json']).then(function(maps) {
-        let map = maps[0];
+      let images = map.tilesets.map((tileset) => ({
+          graphics: [tileset.image],
+          spritesheet: {
+            width: tileset.tileWidth,
+            height: tileset.tileHeight,
+            offsetX: tileset.offsetX || 0,
+            offsetY: tileset.offsetY || 0,
+            spacing: tileset.spacing || 0
+          }
+        }));
 
-        let images = map.tilesets.map((tileset) => ({
-            graphics: [tileset.image],
-            spritesheet: {
-              width: tileset.tileWidth,
-              height: tileset.tileHeight,
-              offsetX: tileset.offsetX || 0,
-              offsetY: tileset.offsetY || 0,
-              spacing: tileset.spacing || 0
-            }
-          }));
+      imgLoader(images).then(function(imgResponse) {
+        let tileEngine = new TileEngine(0, 0, map.height, map.width);
 
-        imgLoader(images).then(function(imgResponse) {
-          let tileEngine = new TileEngine(0, 0, map.height, map.width);
+        let layers = map.layers.map((layer) => Object.assign(layer, {
+        	graphics: imgResponse[layer.tileset].files,
+          graphicsDictionary: imgResponse[layer.tileset].dictionary,
+          zeroIsBlank: true,
+          isometric: false,
+          tileWidth: map.tileWidth,
+          tileHeight: map.tileHeight
+        }));
 
-          let layers = map.layers.map((layer) => Object.assign(layer, {
-          	graphics: imgResponse[layer.tileset].files,
-            graphicsDictionary: imgResponse[layer.tileset].dictionary,
-            zeroIsBlank: true,
-            isometric: false,
-            tileWidth: map.tileWidth,
-            tileHeight: map.tileHeight
-          }));
-
-          tileEngine.init(layers);
-        });
+        tileEngine.init(layers);
       });
-    }
-
-    launch();
+    });
   });

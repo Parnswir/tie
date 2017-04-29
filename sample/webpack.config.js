b4ab8c21bd0fbd0f09aa7e71fb9d6460bfb07310
Parnswir <maximiliangrundke@googlemail.com>
2017-04-29 17:04:43 +0200

    Add babel pipeline to webpack config

--- sample/webpack.config.js
+++ sample/webpack.config.js
@@ -6,6 +6,18 @@ module.exports = {
   entry: {
     app: './index.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'babel-loader',
+          options: { presets: ['es2015'] },
+        }],
+      }
+    ]
+  },
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].bundle.js',

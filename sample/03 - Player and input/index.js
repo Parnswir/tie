85394faa9d52f8474ca1cac150f652e9958444bd
Parnswir <maximiliangrundke@googlemail.com>
2017-05-03 18:31:13 +0200

    Add character example

---
+++ sample/03 - Player and input/index.js
@@ -0,0 +1,10 @@
+import TileEngine from '../../src/engine';
+import MapLoader from '../../src/map';
+
+(new MapLoader()).load('./map.json').then((map) => {
+    let tileEngine = new TileEngine(0, 0, map.height, map.width, {
+      enableKeyboardInput: true,
+      enableMouseInput: true
+    });
+    tileEngine.init(map);
+});

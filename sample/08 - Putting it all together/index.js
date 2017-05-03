cd48fa69aaa07eae24d4922fb21dce9b5f2c33ef
Parnswir <maximiliangrundke@googlemail.com>
2017-05-03 14:04:19 +0200

    Enable in and output

--- sample/08 - Putting it all together/index.js
+++ sample/08 - Putting it all together/index.js
@@ -3,6 +3,11 @@ import MapLoader from '../../src/map';
 
 let loader = new MapLoader();
 loader.load('./assets/maps/house.json').then((map) => {
-    let tileEngine = new TileEngine(0, 0, map.height, map.width, document.body, {lockedFrameRate: false});
+    let tileEngine = new TileEngine(0, 0, map.height, map.width, document.body, {
+      lockedFrameRate: false,
+      enableKeyboardInput: true,
+      enableMouseInput: true,
+      enableTextOutput: true
+    });
     tileEngine.init(map);
 });

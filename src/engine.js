b45688b83f339a8e0f315105a6187ce6424725cb
Maximilian Grundke <maximiliangrundke@googlemail.com>
2017-06-04 18:42:51 +0200

    Classify Engine

--- src/engine.js
+++ src/engine.js
@@ -46,266 +46,269 @@ let createElements = (container, names, outputEnabled=false) => {
   appendHtml(container, elements);
 }
 
-export default function TileEngine (x, y, xrange, yrange, overrides) {
-  let self = this;
-  overrides = Object.assign({}, overrides);
+export default class TileEngine {
 
-  let parent = overrides.parent || document.body;
-  let elementNames = Object.assign(ELEMENT_NAMES, overrides.customElementNames);
-  if (!overrides.useCustomElements) {
-    createElements(parent, elementNames, overrides.enableTextOutput);
-  }
-  const container = document.getElementById(elementNames.containerName);
+  constructor(x, y, xrange, yrange, overrides) {
+    let self = this;
+    overrides = Object.assign({}, overrides);
 
-  let textMessages = [];
-  const textMessageFrame = document.getElementById(elementNames.frameName);
-  const textMessage = document.getElementById(elementNames.messageName);
-  const textIndicator = document.getElementById(elementNames.indicatorName)
+    let parent = overrides.parent || document.body;
+    let elementNames = Object.assign(ELEMENT_NAMES, overrides.customElementNames);
+    if (!overrides.useCustomElements) {
+      createElements(parent, elementNames, overrides.enableTextOutput);
+    }
+    const container = document.getElementById(elementNames.containerName);
 
-  const controlWidth = container.clientWidth;
-  const controlHeight = container.clientHeight;
-  const context = CanvasControl.create("canvas", controlWidth, controlHeight, {}, elementNames.containerName, true);
+    let textMessages = [];
+    const textMessageFrame = document.getElementById(elementNames.frameName);
+    const textMessage = document.getElementById(elementNames.messageName);
+    const textIndicator = document.getElementById(elementNames.indicatorName)
 
-  let currentMap;
+    const controlWidth = container.clientWidth;
+    const controlHeight = container.clientHeight;
+    const context = CanvasControl.create("canvas", controlWidth, controlHeight, {}, elementNames.containerName, true);
 
-  let paused = false;
-  let mapLayers = [];
-  let players = [];
-  let playerMap = {};
+    let currentMap;
 
-  let actionExecutor = new ActionExecutor();
-  actionExecutor.registerAction('toggleTile', (options, engine, player) => {
-    let layers = mapLayers.filter((layer) => layer.zIndex === player.properties.zIndex);
-    layers.forEach((layer) => {
-      let currentTile = layer.getTile(options.target.x, options.target.y);
-      let currentIndex = options.tiles.indexOf(currentTile);
-      layer.setTile(options.target.x, options.target.y, options.tiles[(currentIndex + 1) % options.tiles.length]);
-    })
-  });
+    let paused = false;
+    let mapLayers = [];
+    let players = [];
+    let playerMap = {};
 
-  let clearText = function () {};
-  let displayText = function () {};
-  let drawMessages = function () {};
+    let actionExecutor = new ActionExecutor();
+    actionExecutor.registerAction('toggleTile', (options, engine, player) => {
+      let layers = mapLayers.filter((layer) => layer.zIndex === player.properties.zIndex);
+      layers.forEach((layer) => {
+        let currentTile = layer.getTile(options.target.x, options.target.y);
+        let currentIndex = options.tiles.indexOf(currentTile);
+        layer.setTile(options.target.x, options.target.y, options.tiles[(currentIndex + 1) % options.tiles.length]);
+      })
+    });
 
-  if (overrides.enableTextOutput) {
-    textMessageFrame.onclick = () => {
-      if (paused) {
-        drawMessages();
+    let clearText = function () {};
+    let displayText = function () {};
+    let drawMessages = function () {};
+
+    if (overrides.enableTextOutput) {
+      textMessageFrame.onclick = () => {
+        if (paused) {
+          drawMessages();
+        }
+      };
+
+      clearText = () => {
+        textMessages = [];
+        textMessageFrame.classList.remove("in");
+        textMessage.innerHTML = "";
+        textIndicator.classList.remove("in");
+        if (paused) {
+          unpause();
+        }
+      }
+
+      displayText = (text) => {
+        textMessages = textMessages.concat(text);
+      }
+
+      drawMessages = () => {
+        if (textMessages.length > 0) {
+          pause();
+          let text = textMessages.splice(0, 1)[0];
+          textMessage.innerHTML = text;
+          if (!("in" in textMessageFrame.classList)) {
+            textMessageFrame.classList.add("in");
+          }
+          if (textMessages.length >= 1) {
+            textIndicator.classList.add("in");
+          } else {
+            textIndicator.classList.remove("in");
+          }
+        } else {
+          clearText();
+        }
+      }
+    }
+
+    const input = new CanvasInput(document, CanvasControl());
+    if (overrides.enableMouseInput) {
+      input.mouse_action(function(coords) {
+        if (paused) {
+          drawMessages()
+        } else {
+          let player = getCharacter('player');
+          let layer = player.properties.layer;
+          let t = layer.applyMouseFocus(coords.x, coords.y);
+          player.goTo(t.x, t.y);
+          if (Math.abs(t.x - player.getTile().x) + Math.abs(t.y - player.getTile().y) === 1) {
+            interact(player, t);
+          }
+        }
+      });
+    }
+
+    if (overrides.enableKeyboardInput) {
+      input.keyboard(function(pressed, status) {
+        let player = getCharacter('player');
+        if (status) {
+          if (paused) {
+            if ([13, 32].indexOf(pressed) >= 0) {
+              drawMessages();
+            }
+          } else {
+            switch (pressed) {
+              case 37: player.moveTo(player.getTile().x - 1, player.getTile().y); break;
+              case 38: player.moveTo(player.getTile().x, player.getTile().y - 1); break;
+              case 39: player.moveTo(player.getTile().x + 1, player.getTile().y); break;
+              case 40: player.moveTo(player.getTile().x, player.getTile().y + 1); break;
+              case 13: case 32: interact(player, player.getLookedAtTile()); break;
+            }
+          }
+        }
+      });
+    }
+
+    let getActions = () => {
+      return currentMap.actions || [];
+    }
+
+    let interact = (player, tile) => {
+      if (!player.isMoving()) {
+        getActions()
+          .filter((action) => action.type !== actionExecutor.TYPE_POSITIONAL)
+          .filter((action) => action.x === tile.x && action.y == tile.y)
+          .forEach((action) => actionExecutor.execute(action, self, player));
       }
     };
 
-    clearText = () => {
-      textMessages = [];
-      textMessageFrame.classList.remove("in");
-      textMessage.innerHTML = "";
-      textIndicator.classList.remove("in");
+    let pause = () => {
+      paused = true;
+    }
+
+    let unpause = () => {
       if (paused) {
-        unpause();
+        paused = false;
+        draw();
       }
     }
 
-    displayText = (text) => {
-      textMessages = textMessages.concat(text);
+    let getCharacter = (id) => playerMap[id];
+
+    let setPlayerLighting = (tile) => {
+      mapLayers.forEach((layer) => layer.setLight(tile.x, tile.y));
     }
 
-    drawMessages = () => {
-      if (textMessages.length > 0) {
-        pause();
-        let text = textMessages.splice(0, 1)[0];
-        textMessage.innerHTML = text;
-        if (!("in" in textMessageFrame.classList)) {
-          textMessageFrame.classList.add("in");
-        }
-        if (textMessages.length >= 1) {
-          textIndicator.classList.add("in");
-        } else {
-          textIndicator.classList.remove("in");
-        }
-      } else {
-        clearText();
-      }
-    }
-  }
-
-  const input = new CanvasInput(document, CanvasControl());
-  if (overrides.enableMouseInput) {
-    input.mouse_action(function(coords) {
-      if (paused) {
-        drawMessages()
-      } else {
-        let player = getCharacter('player');
-        let layer = player.properties.layer;
-        let t = layer.applyMouseFocus(coords.x, coords.y);
-        player.goTo(t.x, t.y);
-        if (Math.abs(t.x - player.getTile().x) + Math.abs(t.y - player.getTile().y) === 1) {
-          interact(player, t);
+    let drawLayer = (layer) => {
+      for (let i = 0; i < (layer.width || xrange); i++) {
+        for (let j = 0; j < (layer.height || yrange); j++) {
+          layer.draw(i, j, void 0, (layer.x || x), (layer.y || y));
         }
       }
-    });
-  }
-
-  if (overrides.enableKeyboardInput) {
-    input.keyboard(function(pressed, status) {
-      let player = getCharacter('player');
-      if (status) {
-        if (paused) {
-          if ([13, 32].indexOf(pressed) >= 0) {
-            drawMessages();
-          }
-        } else {
-          switch (pressed) {
-            case 37: player.moveTo(player.getTile().x - 1, player.getTile().y); break;
-            case 38: player.moveTo(player.getTile().x, player.getTile().y - 1); break;
-            case 39: player.moveTo(player.getTile().x + 1, player.getTile().y); break;
-            case 40: player.moveTo(player.getTile().x, player.getTile().y + 1); break;
-            case 13: case 32: interact(player, player.getLookedAtTile()); break;
-          }
-        }
-      }
-    });
-  }
-
-  let getActions = () => {
-    return currentMap.actions || [];
-  }
-
-  let interact = (player, tile) => {
-    if (!player.isMoving()) {
-      getActions()
-        .filter((action) => action.type !== actionExecutor.TYPE_POSITIONAL)
-        .filter((action) => action.x === tile.x && action.y == tile.y)
-        .forEach((action) => actionExecutor.execute(action, self, player));
     }
-  };
 
-  let pause = () => {
-    paused = true;
-  }
+    let previousTime = 0;
+    let timeToDraw = (time) => !overrides.lockedFrameRate || (time - previousTime) >= 1000 / overrides.lockedFrameRate;
 
-  let unpause = () => {
-    if (paused) {
-      paused = false;
-      draw();
-    }
-  }
-
-  let getCharacter = (id) => playerMap[id];
-
-  let setPlayerLighting = (tile) => {
-    mapLayers.forEach((layer) => layer.setLight(tile.x, tile.y));
-  }
-
-  let drawLayer = (layer) => {
-    for (let i = 0; i < (layer.width || xrange); i++) {
-      for (let j = 0; j < (layer.height || yrange); j++) {
-        layer.draw(i, j, void 0, (layer.x || x), (layer.y || y));
-      }
-    }
-  }
-
-  let previousTime = 0;
-  let timeToDraw = (time) => !overrides.lockedFrameRate || (time - previousTime) >= 1000 / overrides.lockedFrameRate;
-
-  let draw = (time) => {
-    if (!timeToDraw(time)) {
-      requestAnimationFrame(draw);
-    } else {
-      previousTime = time;
-      context.clearRect(0, 0, controlWidth, controlHeight);
-      let thingsToDraw = mapLayers.concat(players).sort((a, b) => a.zIndex > b.zIndex);
-      for (let thing of thingsToDraw) {
-        if (thing instanceof Player) {
-          thing.draw();
-          thing.move();
-          if (thing.useLighting) {
-            setPlayerLighting(thing.getTile());
-          }
-        } else {
-          if (thing.visible) {
-            drawLayer(thing);
-          }
-        }
-      }
-      drawMessages();
-      if (!paused) {
+    let draw = (time) => {
+      if (!timeToDraw(time)) {
         requestAnimationFrame(draw);
-      }
-    }
-  }
-
-  let initLayer = (layer) => {
-    let mapLayer = new TileField(context, controlWidth, controlHeight);
-    mapLayer.setup(layer);
-    mapLayer.flip("horizontal");
-    mapLayer.rotate("left");
-    mapLayer.setLightmap(layer.lightmap);
-    mapLayer = Object.assign(mapLayer, layer);
-    return mapLayer;
-  }
-
-  let computeOnce = (fn) => {
-    var instance = void 0;
-    return function () {
-      if (instance === void 0) {
-        instance = fn.apply(this, arguments);
-      }
-      return instance;
-    }
-  }
-
-  let createEmptyLayer = computeOnce((map) => {
-    let layer = {
-      width: map.width,
-      height: map.height,
-      layout: Array(map.width * map.height).fill(0)
-    }
-    return initLayer(layer);
-  });
-
-  let init = (map) => {
-    currentMap = map;
-    mapLayers = map.layers.map(initLayer);
-    draw();
-    let promises = [];
-    let characters = map.characters || {};
-    for (let characterId of Object.keys(characters)) {
-      let playerOptions = characters[characterId];
-      if (playerOptions) {
-        promises.push(imgLoader([{
-          graphics: [playerOptions.sprites],
-          spritesheet: {
-            width: playerOptions.width,
-            height: playerOptions.height
+      } else {
+        previousTime = time;
+        context.clearRect(0, 0, controlWidth, controlHeight);
+        let thingsToDraw = mapLayers.concat(players).sort((a, b) => a.zIndex > b.zIndex);
+        for (let thing of thingsToDraw) {
+          if (thing instanceof Player) {
+            thing.draw();
+            thing.move();
+            if (thing.useLighting) {
+              setPlayerLighting(thing.getTile());
+            }
+          } else {
+            if (thing.visible) {
+              drawLayer(thing);
+            }
           }
-        }]).then((playerImages) => {
-          playerOptions.files = playerImages[0].files;
-          playerOptions.layer = mapLayers[0];
-          playerOptions.pathfindingLayer = mapLayers[playerOptions.pathfindingLayer] || createEmptyLayer(map);
-          playerOptions.tileWidth = map.tileWidth;
-          playerOptions.tileHeight = map.tileHeight;
-
-          let player = new Player(context, playerOptions, playerOptions.x, playerOptions.y, pathfind);
-          player.on("changeTile", (p, tile) => {
-            getActions().filter((action) => action.type === actionExecutor.TYPE_POSITIONAL).forEach((action) => {
-              if (action.x === tile.x && action.y === tile.y) {
-                actionExecutor.execute(action, self, p);
-              }
-            })
-          });
-          players.push(player);
-          playerMap[characterId] = player;
-        }));
+        }
+        drawMessages();
+        if (!paused) {
+          requestAnimationFrame(draw);
+        }
       }
     }
-    return Promise.all(promises);
+
+    let initLayer = (layer) => {
+      let mapLayer = new TileField(context, controlWidth, controlHeight);
+      mapLayer.setup(layer);
+      mapLayer.flip("horizontal");
+      mapLayer.rotate("left");
+      mapLayer.setLightmap(layer.lightmap);
+      mapLayer = Object.assign(mapLayer, layer);
+      return mapLayer;
+    }
+
+    let computeOnce = (fn) => {
+      var instance = void 0;
+      return function () {
+        if (instance === void 0) {
+          instance = fn.apply(this, arguments);
+        }
+        return instance;
+      }
+    }
+
+    let createEmptyLayer = computeOnce((map) => {
+      let layer = {
+        width: map.width,
+        height: map.height,
+        layout: Array(map.width * map.height).fill(0)
+      }
+      return initLayer(layer);
+    });
+
+    let init = (map) => {
+      currentMap = map;
+      mapLayers = map.layers.map(initLayer);
+      draw();
+      let promises = [];
+      let characters = map.characters || {};
+      for (let characterId of Object.keys(characters)) {
+        let playerOptions = characters[characterId];
+        if (playerOptions) {
+          promises.push(imgLoader([{
+            graphics: [playerOptions.sprites],
+            spritesheet: {
+              width: playerOptions.width,
+              height: playerOptions.height
+            }
+          }]).then((playerImages) => {
+            playerOptions.files = playerImages[0].files;
+            playerOptions.layer = mapLayers[0];
+            playerOptions.pathfindingLayer = mapLayers[playerOptions.pathfindingLayer] || createEmptyLayer(map);
+            playerOptions.tileWidth = map.tileWidth;
+            playerOptions.tileHeight = map.tileHeight;
+
+            let player = new Player(context, playerOptions, playerOptions.x, playerOptions.y, pathfind);
+            player.on("changeTile", (p, tile) => {
+              getActions().filter((action) => action.type === actionExecutor.TYPE_POSITIONAL).forEach((action) => {
+                if (action.x === tile.x && action.y === tile.y) {
+                  actionExecutor.execute(action, self, p);
+                }
+              })
+            });
+            players.push(player);
+            playerMap[characterId] = player;
+          }));
+        }
+      }
+      return Promise.all(promises);
+    }
+
+    this.init = init;
+    this.displayText = displayText;
+    this.pause = pause;
+    this.unpause = unpause;
+    this.getCharacter = getCharacter;
+
+    this.actionExecutor = actionExecutor;
+    this.currentMap = currentMap;
   }
-
-  this.init = init;
-  this.displayText = displayText;
-  this.pause = pause;
-  this.unpause = unpause;
-  this.getCharacter = getCharacter;
-
-  this.actionExecutor = actionExecutor;
-  this.currentMap = currentMap;
 }

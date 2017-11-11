f284bf8020f94c60e9dc027be03c505383ac3e4f
Maximilian Grundke <maximiliangrundke@googlemail.com>
2017-11-11 16:13:33 +0100

    Add more tests

--- test/EventEmitter.js
+++ test/EventEmitter.js
@@ -30,3 +30,41 @@ test('supplies an event object to the callback', t => {
   });
   emitter.createEvent('test', original);
 });
+
+test('handles events with "once:" prefix only once', t => {
+  t.plan(1);
+  const emitter = newEventEmitter();
+  emitter.on('once:test', () => t.pass());
+  emitter.createEvent('test');
+  emitter.createEvent('test');
+});
+
+test('can unregister event handlers in bulk', t => {
+  t.plan(1);
+  const emitter = newEventEmitter();
+  emitter.on('test', () => t.pass());
+  emitter.createEvent('test');
+  emitter.off('test');
+  emitter.createEvent('test');
+});
+
+test('can unregister event handlers by id', t => {
+  t.plan(1);
+  const emitter = newEventEmitter();
+  const id = emitter.on('test', () => t.pass());
+  emitter.createEvent('test');
+  emitter.off('test', id);
+  emitter.createEvent('test');
+});
+
+test('does not affect other events when unregistering', t => {
+  t.plan(3);
+  const emitter = newEventEmitter();
+  emitter.on('foo', () => t.pass());
+  emitter.on('bar', () => t.pass());
+  emitter.createEvent('foo');
+  emitter.createEvent('bar');
+  emitter.off('foo');
+  emitter.createEvent('foo');
+  emitter.createEvent('bar');
+});

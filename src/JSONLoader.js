005f4d5afc6512d3efd98dffeb7fc160f77a59b2
Maximilian Grundke <maximiliangrundke@googlemail.com>
2017-11-17 23:43:18 +0100

    Refactor JSONLoader

--- src/JSONLoader.js
+++ src/JSONLoader.js
@@ -1,37 +1,41 @@
 import {merge} from './util';
 import {getResource} from './request';
 
+const _loadNested = async function (obj, maxDepth) {
+  for (let property in obj) {
+    if (obj.hasOwnProperty(property) && typeof(obj[property]) === 'object') {
+      if (obj[property][JSONLoader.LOAD_PROPERTY()]) {
+        let child = await _load(obj[property][JSONLoader.LOAD_PROPERTY()], maxDepth - 1);
+        if (obj[property][JSONLoader.OVERRIDE_PROPERTY()]) {
+          child = merge(child, obj[property][JSONLoader.OVERRIDE_PROPERTY()]);
+        }
+        obj[property] = child;
+      } else {
+        obj[property] = await _loadNested(obj[property], maxDepth);
+      }
+    }
+  }
+  return obj;
+}
+
+const _load = (path, maxDepth=JSONLoader.MAX_MERGE_DEPTH()) => {
+  if (maxDepth <= 0) {
+    throw 'Too many nested JSON merges!';
+  }
+  return getResource(path)
+    .then((obj) => _loadNested(obj, maxDepth))
+    .catch((error) => {throw `Could not load JSON file at ${path}.\n - ${error}`});
+}
+
 export default class JSONLoader {
 
   constructor() {}
 
+  static LOAD_PROPERTY() {return '_load';}
+  static OVERRIDE_PROPERTY() {return '_override';}
+  static MAX_MERGE_DEPTH() {return 10;}
+
   static load(...paths) {
-
-    function _load(path, maxDepth=10) {
-      let _loadNested = async function (obj, maxDepth) {
-        for (let property in obj) {
-          if (obj.hasOwnProperty(property) && typeof(obj[property]) === 'object') {
-            if (obj[property]['_load']) {
-              let child = await _load(obj[property]['_load'], maxDepth - 1);
-              if (obj[property]['_override']) {
-                child = merge(child, obj[property]['_override']);
-              }
-              obj[property] = child;
-            } else {
-              obj[property] = await _loadNested(obj[property], maxDepth);
-            }
-          }
-        }
-        return obj;
-      }
-      if (maxDepth <= 0) {
-        throw 'Too many nested JSON merges!';
-      }
-      return getResource(path)
-        .then((obj) => _loadNested(obj, maxDepth))
-        .catch((error) => {throw `Could not load JSON file at ${path}.\n - ${error}`});
-    }
-
     let promises = [];
     for (let i = 0; i < paths.length; i++) {
       promises.push(_load(paths[i]));
